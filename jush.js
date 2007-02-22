34e894a605d80a16b4245a3f32abda06e8c01067
jakubvrana <jakubvrana@9d3eea12-fc29-0410-b379-a5151d7be1fb>
2007-02-22 12:46:36 +0000

    Add URL
    git-svn-id: https://jush.svn.sourceforge.net/svnroot/jush/trunk@11 9d3eea12-fc29-0410-b379-a5151d7be1fb

--- jush.js
+++ jush.js
@@ -1,4 +1,4 @@
-// Copyright 2007 Jakub Vrana, licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// Copyright 2007 Jakub Vrana http://jush.info, licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
 
 /* Limitations:
 <style> and <script> supposes CDATA or HTML comments
