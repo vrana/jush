d9e8bae1398bc0847b1bf1bda6eb8a99d4e8742c
jakubvrana <jakubvrana@9d3eea12-fc29-0410-b379-a5151d7be1fb>
2009-01-29 11:28:10 +0000

    Parse <script language=php> in htm
    git-svn-id: https://jush.svn.sourceforge.net/svnroot/jush/trunk@86 9d3eea12-fc29-0410-b379-a5151d7be1fb

--- jush.js
+++ jush.js
@@ -93,10 +93,10 @@ var jush = {
 	},
 	
 	highlight_states: function (states, text, in_php, escape) {
-		var php = /<\?(?:php)?|<script\s+language\s*=\s*(?:"php"|'php'|php)\s*>/i; // asp_tags=0, short_open_tag=1
+		var php = /<\?(?!xml)(?:php)?|<script\s+language\s*=\s*(?:"php"|'php'|php)\s*>/i; // asp_tags=0, short_open_tag=1
 		var num = /(?:\b[0-9]+\.?[0-9]*|\.[0-9]+)(?:[eE][+-]?[0-9]+)?/;
 		var tr = { // transitions
-			htm: { tag_css: /(<)(style)\b/i, tag_js: /(<)(script)\b/i, htm_com: /<!--/, 0: /(<!)([^>]*)(>)/, tag: /(<)([^<>?\s]+|\?xml)/, php: php, ent: /&/ },
+			htm: { php: php, tag_css: /(<)(style)\b/i, tag_js: /(<)(script)\b/i, htm_com: /<!--/, 0: /(<!)([^>]*)(>)/, tag: /(<)([^<>\s]+)/, ent: /&/ },
 			htm_com: { php: php, 1: /-->/ },
 			ent: { php: php, 1: /;/ },
 			tag: { php: php, att_css: /(\s+)(style)(\s*=\s*)/i, att_js: /(\s+)(on[^=<>\s]+)(\s*=\s*)/i, att: /(\s+)([^=<>\s]*)(\s*)/, 1: />/ },
