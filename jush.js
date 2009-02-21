980b4a4612fe6efafcb344d04a03eb15268bdb87
jakubvrana <jakubvrana@9d3eea12-fc29-0410-b379-a5151d7be1fb>
2009-02-21 05:54:14 +0000

    Highlight <br/> (bug #2231378)
    git-svn-id: https://jush.svn.sourceforge.net/svnroot/jush/trunk@87 9d3eea12-fc29-0410-b379-a5151d7be1fb

--- jush.js
+++ jush.js
@@ -96,7 +96,7 @@ var jush = {
 		var php = /<\?(?!xml)(?:php)?|<script\s+language\s*=\s*(?:"php"|'php'|php)\s*>/i; // asp_tags=0, short_open_tag=1
 		var num = /(?:\b[0-9]+\.?[0-9]*|\.[0-9]+)(?:[eE][+-]?[0-9]+)?/;
 		var tr = { // transitions
-			htm: { php: php, tag_css: /(<)(style)\b/i, tag_js: /(<)(script)\b/i, htm_com: /<!--/, 0: /(<!)([^>]*)(>)/, tag: /(<)([^<>\s]+)/, ent: /&/ },
+			htm: { php: php, tag_css: /(<)(style)\b/i, tag_js: /(<)(script)\b/i, htm_com: /<!--/, 0: /(<!)([^>]*)(>)/, tag: /(<\/?)([^<>\s\/]+)/, ent: /&/ },
 			htm_com: { php: php, 1: /-->/ },
 			ent: { php: php, 1: /;/ },
 			tag: { php: php, att_css: /(\s+)(style)(\s*=\s*)/i, att_js: /(\s+)(on[^=<>\s]+)(\s*=\s*)/i, att: /(\s+)([^=<>\s]*)(\s*)/, 1: />/ },
