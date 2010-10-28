8edeeb7208f75b5868e34c2108eb2deb39036a54
Jakub Vrana <jakub@vrana.cz>
2010-10-29 00:27:25 +0200

    Link

--- jush.js
+++ jush.js
@@ -1,6 +1,6 @@
 /** JUSH - JavaScript Syntax Highlighter
 * @link http://jush.sourceforge.net
-* @author Jakub Vrana, http://php.vrana.cz
+* @author Jakub Vrana, http://www.vrana.cz
 * @copyright 2007 Jakub Vrana
 * @license http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
 */
