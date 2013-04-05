e19e1fbd00de59d58c9ac0552f5048389ab5a9ff
Jakub Vrana <jakub@vrana.cz>
2013-04-05 09:08:53 -0700

    Help cursor for documentation links

--- jush.js
+++ jush.js
@@ -84,7 +84,12 @@ var jush = {
 	},
 
 	create_link: function (link, s, attrs) {
-		return '<a' + (this.create_links && link ? ' href="' + link + '"' : '') + (typeof this.create_links == 'string' ? this.create_links : '') + (attrs ? attrs : '') + '>' + s + '</a>';
+		return '<a'
+			+ (this.create_links && link ? ' href="' + link + '" class="jush-help"' : '')
+			+ (typeof this.create_links == 'string' ? this.create_links : '')
+			+ (attrs || '')
+			+ '>' + s + '</a>'
+		;
 	},
 
 	keywords_links: function (state, s) {
